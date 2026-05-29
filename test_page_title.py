67d7d65072a481b58964edb8f1753853f914d4f9
Jayanth N Gowda <108566111+Jayanthjai@users.noreply.github.com>
2026-05-29 15:12:26 +0530

    added print

--- test_page_title.py
+++ test_page_title.py
@@ -4,4 +4,6 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("https://testautomationpractice.blogspot.com/")
 title=driver.title
+
+print(driver.title)
 assert title=='Automation Testing Practice'
