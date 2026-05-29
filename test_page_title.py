33444e492b0a51570e5f17eb4e20c39cf384405a
Jayanth N Gowda <108566111+Jayanthjai@users.noreply.github.com>
2026-05-29 14:42:25 +0530

    commited the test file for repo

---
+++ test_page_title.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("https://testautomationpractice.blogspot.com/")
+title=driver.title()
+assert title=='Automation Testing Practice'
