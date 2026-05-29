3bb79bd86bff4a1233a092a1babfe972c5aed81b
Jayanth N Gowda <108566111+Jayanthjai@users.noreply.github.com>
2026-05-29 15:22:09 +0530

    added statement in print

--- test_page_title.py
+++ test_page_title.py
@@ -5,6 +5,6 @@ driver = webdriver.Chrome()
 driver.get("https://testautomationpractice.blogspot.com/")
 title=driver.title
 
-print(driver.title)
+print('the title is: ', driver.title)
 def test_title():
   assert title=='Automation Testing Practice'
